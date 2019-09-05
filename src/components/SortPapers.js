0db150e20c8ec138b89ce08e07c3a6f301f0460b
github username <lawrence@sciencedisrupt.com>
2019-09-05 10:46:24 +0100

    removed alphabetical asc and desc sort for papers

--- src/components/SortPapers.js
+++ src/components/SortPapers.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 
 const SortPapers = ({ sortPapers }) => {
   return (
     <select onChange={e => sortPapers(e.target.value)}>
-      <option value="Ascending">title ascending</option>
-      <option value="Descending">title descending</option>
+      {/* <option value="Ascending">title ascending</option>
+      <option value="Descending">title descending</option> */}
       <option value="Rating">rating</option>
       <option value="Debated">debated</option>
       <option value="Latest">latest</option>
