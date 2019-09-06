70c410e66cae976d056567ef83129f9f28b10bdc
github username <lawrence@sciencedisrupt.com>
2019-09-06 23:45:59 +0100

    refactored menu component with new ternary for signed in users

--- src/views/Menu.js
+++ src/views/Menu.js
@@ -12,12 +12,12 @@ class Menu extends Component {
     const { history } = this.props;
     console.log("🔍 searching for things ...", input);
     // push state with ?query
-    this.props.showMenu()
+    this.props.showMenu();
     history.push(`/papers/search/${input.split(" ").join("-")}`);
   };
 
   handleChange = e => {
-    const {value} = e.target
+    const { value } = e.target;
     this.setState({
       input: value
     });
@@ -26,67 +26,65 @@ class Menu extends Component {
   render() {
     const { user } = this.props;
     const profilePath = `/users/${user.user_id}`;
-    const view =
-      user.user_id !== null ? ( // the menu to be rendered when signed in
-        <div className="drop-menu">
-          <ul id="menu">
-            <form onSubmit={this.handleSubmit}>
-              <input
-                value={this.state.input}
-                type="text"
-                placeholder="search papers ..."
-                name="search"
-                onChange={this.handleChange}
-              />
-            </form>
-            <NavLink
-              exact
-              to="/"
-              activeStyle={{
-                fontWeight: "bold",
-                color: "#f9009a"
-              }}
-              onClick={this.props.showMenu}
-            >
-              <li>
-                Home
-                <span role="img" aria-label="Home">
-                  🏠
-                </span>
-              </li>
-            </NavLink>
-            <NavLink
-              to={profilePath}
-              activeStyle={{
-                fontWeight: "bold",
-                color: "#f9009a"
-              }}
-              onClick={this.props.showMenu}
-            >
-              <li>
-                Profile
-                <span role="img" aria-label="Profile">
-                  👩‍🔬
-                </span>
-              </li>
-            </NavLink>
-
-            <NavLink
-              to="/papers"
-              activeStyle={{
-                fontWeight: "bold",
-                color: "#f9009a"
-              }}
-              onClick={this.props.showMenu}
-            >
-              <li>
-                Papers
-                <span role="img" aria-label="Papers">
-                  📖
-                </span>
-              </li>
-            </NavLink>
-
+    const view = (
+      <div className="drop-menu">
+        <ul id="menu">
+          <form onSubmit={this.handleSubmit}>
+            <input
+              value={this.state.input}
+              type="text"
+              placeholder="search papers ..."
+              name="search"
+              onChange={this.handleChange}
+            />
+          </form>
+          <NavLink
+            exact
+            to="/"
+            activeStyle={{
+              fontWeight: "bold",
+              color: "#f9009a"
+            }}
+            onClick={this.props.showMenu}
+          >
+            <li>
+              Home
+              <span role="img" aria-label="Home">
+                🏠
+              </span>
+            </li>
+          </NavLink>
+          <NavLink
+            to={profilePath}
+            activeStyle={{
+              fontWeight: "bold",
+              color: "#f9009a"
+            }}
+            onClick={this.props.showMenu}
+          >
+            <li>
+              Profile
+              <span role="img" aria-label="Profile">
+                👩‍🔬
+              </span>
+            </li>
+          </NavLink>
+          <NavLink
+            to="/papers"
+            activeStyle={{
+              fontWeight: "bold",
+              color: "#f9009a"
+            }}
+            onClick={this.props.showMenu}
+          >
+            <li>
+              Papers
+              <span role="img" aria-label="Papers">
+                📖
+              </span>
+            </li>
+          </NavLink>
+          {user.user_id !== null ? (
             <NavLink
               activeStyle={{
                 fontWeight: "bold",
@@ -104,48 +102,50 @@ class Menu extends Component {
                 </span>
               </li>
             </NavLink>
-          </ul>
-        </div>
-      ) : (
-        // the menu to be rendered when not signed in
-        <div className="drop-menu">
-          <ul id="menu">
-            {/* <input type="text" placeholder="search papers ..."></input> */}
-            <NavLink
-              exact
-              to="/"
-              activeStyle={{
-                fontWeight: "bold",
-                color: "#f9009a"
-              }}
-              onClick={this.props.showMenu}
-            >
-              <li>
-                Home
-                <span role="img" aria-label="Home">
-                  🏠
-                </span>
-              </li>
-            </NavLink>
+          ) : (
+            <Fragment>
+              <NavLink
+                to="/signin"
+                activeStyle={{
+                  fontWeight: "bold",
+                  color: "#f9009a"
+                }}
+                onClick={() => {
+                  this.props.showMenu();
+                }}
+              >
+                <li>
+                  Sign In
+                  <span role="img" aria-label="Sign In">
+                    👋
+                  </span>
+                </li>
+              </NavLink>
+              <NavLink
+                to="/signup"
+                activeStyle={{
+                  fontWeight: "bold",
+                  color: "#f9009a"
+                }}
+                onClick={() => {
+                  this.props.showMenu();
+                }}
+              >
+                <li>
+                  Sign Up
+                  <span role="img" aria-label="Sign Up">
+                    👋
+                  </span>
+                </li>
+              </NavLink>
+            </Fragment>
+          )}
+        </ul>
+      </div>
+    );
 
-            <NavLink
-              to="/papers"
-              activeStyle={{
-                fontWeight: "bold",
-                color: "#f9009a"
-              }}
-              onClick={this.props.showMenu}
-            >
-              <li>
-                Papers
-                <span role="img" aria-label="Papers">
-                  📖
-                </span>
-              </li>
-            </NavLink>
-          </ul>
-        </div>
-      );
+    // the menu to be rendered when not signed in
+
     return <Fragment>{view}</Fragment>;
   }
 }
