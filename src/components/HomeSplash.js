804133873e1be39d61740e12ec5e7bf025c567de
github username <lawrence@sciencedisrupt.com>
2019-09-06 16:20:41 +0100

    updated styling of banner image

--- src/components/HomeSplash.js
+++ src/components/HomeSplash.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const HomeSplash = () => {
   return (
     <div className="home-splash">
       <h1>Welcome to T.O.S</h1>
+      <div className="home-splash-image"></div>
+      <div className="home-splash-overlay"></div>
+      <span>scroll</span>
     </div>
   );
 };
