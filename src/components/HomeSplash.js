d835bf46b55e1e342fa9ab7a870be496edcc207c
github username <lawrence@sciencedisrupt.com>
2019-09-06 17:05:20 +0100

    scroll prompt

--- src/components/HomeSplash.js
+++ src/components/HomeSplash.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 const HomeSplash = () => {
   return (
@@ -6,7 +6,9 @@ const HomeSplash = () => {
       <h1>Welcome to T.O.S</h1>
       <div className="home-splash-image"></div>
       <div className="home-splash-overlay"></div>
-      <span>scroll</span>
+      <div className="scroll">
+  
+      </div>
     </div>
   );
 };
