09507aa3b3a2f1f00da9fba9ec94c9b912c536b9
github username <lawrence@sciencedisrupt.com>
2019-09-11 14:38:05 +0100

    filled out the rest of the title container with about copy

--- src/components/BlockThree.js
+++ src/components/BlockThree.js
@@ -1,17 +1,18 @@
 import React from "react";
-const BlockOne = () => {
+const BlockThree = () => {
   return (
     <div className="hpc-block-container">
       <h2 className="hpc-h1">
-        <span className="hpc-span">1. Who?</span>
+        <span className="hpc-span">3. Why?</span>
       </h2>
       <h4 className="hpc-h4">
         <span className="hpc-span">
-          An international community of science disruptors
+          We believe that science is a public good. But it ceases to be so if it
+          is carried out behind closed doors !
         </span>
       </h4>
     </div>
   );
 };
 
-export default BlockOne;
+export default BlockThree;
